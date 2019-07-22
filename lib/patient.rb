b9ac21f7a6c62c75a338199eeda23f0453951701
Hillary Spitzer <hspitzer2@gmail.com>
2019-07-22 20:16:24 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -25,8 +25,8 @@ class Patient
 
 
   def doctors
-   appointments.map do |appointments|
-      appointments.doctor
+   appointments.map do |appointment|
+      appointment.doctor
     end
   end
 end
