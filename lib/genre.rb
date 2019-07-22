12c750b555c06bc25406ae1eb65cf8e92f7b1c66
Hillary Spitzer <hspitzer2@gmail.com>
2019-07-22 17:22:05 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -12,16 +12,18 @@ attr_accessor :song
     @artist = artist
     @@all << self
   end
-
+  
+  def self.all
+    @@all
+  end
+  
   def songs
     Songs.all.select do |songs|
       songs.artists == self
     end
   end
 
-  def self.all
-    @@all
-  end
+  
 
   def artisits
     Song.all.map do |songs|
