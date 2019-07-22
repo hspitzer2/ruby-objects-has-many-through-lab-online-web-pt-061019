981033dc3ed9e3e7016b59ffefd4c88f75270508
Hillary Spitzer <hspitzer2@gmail.com>
2019-07-22 17:27:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,8 +13,8 @@ class Artist
   end
 
   def new_song(name, genre)
-    song = Song.new(name, genre)
-    song.(name, self, genre)
+    song = Song.new(name, self, genre)
+    self.song.last
   end
 
   def songs
