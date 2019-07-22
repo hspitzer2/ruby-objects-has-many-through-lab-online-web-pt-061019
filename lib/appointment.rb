5a176b506b6c484c5bdcd9f8e845c568def1f0b3
Hillary Spitzer <hspitzer2@gmail.com>
2019-07-22 17:49:24 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,10 +1,11 @@
 class Appointment
 
-  attr_accessor :date, :patient
+  attr_accessor :date, :patient, :doctor
 
   def initialize(date, patient)
     @date = date
     @patient = patient
+    @doctor = doctor
     @@ll << self
   end
 
